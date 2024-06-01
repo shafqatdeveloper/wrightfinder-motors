3b292555a4e00e2a7aee0df6e983f211e71612bf
shafqatdeveloper <mshafqat.developer@gmail.com>
2024-06-01 16:57:51 +0500

    dynamic imports

--- shadownix/src/Pages/Home/Home.jsx
+++ shadownix/src/Pages/Home/Home.jsx
@@ -1,6 +1,7 @@
 import React, { Suspense, lazy } from "react";
-import Hero from "../../Components/Hero/Hero";
-import Cars from "../../Components/ExploreCars/Cars";
+// import Hero from "../../Components/Hero/Hero";
+// import Cars from "../../Components/ExploreCars/Cars";
+const Cars = lazy(() => import("../../Components/ExploreCars/Cars"));
 const Facts = lazy(() => import("../../Components/Facts/Facts"));
 const WhyUs = lazy(() => import("../../Components/WhySchooseUs/WhyUs"));
 
@@ -8,7 +9,9 @@ const Home = () => {
   return (
     <div className="bg-gray-200 flex flex-col gap-10 pt-24 pb-12 md:p-10">
       {/* <Hero /> */}
-      <Cars />
+      <Suspense fallback={<div>Loading...</div>}>
+        <Cars />
+      </Suspense>
       <Suspense fallback={<div>Loading...</div>}>
         <Facts />
       </Suspense>
